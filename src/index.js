2d294cddbcd9d2b84d11039933c18ff3dee97de2
Richie Edwards <redwards@madisonlogic.com>
2021-03-04 22:11:39 -0500

    feat(random function): change random function to accept argument and return that many random items
    Make the random function more powerful so that it can return multiple random items in a single
    invocation.
    
    fixes #1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 let starWarsNames = require("./starwars-names.json");
 let uniqueRandomName = require("unique-random-array");
 
+const random = (num=1) => {
+  const getRandomItem = uniqueRandomName(starWarsNames)
+  const result = [];
+  for (let i = 0; i < num; i++) {
+    result.push(getRandomItem());
+  }
+  return result.length === 1 ? result[0] : result;
+}
+
 module.exports = {
   all: starWarsNames,
-  random: uniqueRandomName(starWarsNames)
+  random: random
 };
+
+
--- src/index.test.js
+++ src/index.test.js
@@ -10,15 +10,22 @@ describe('random', () => {
     test('is a string', () => {
         let result = sw.random();
         expect(typeof result).toBe('string');
-    })
+    });
     
     test('is not the same as before', () => {
         let first = sw.random();
         let second = sw.random();
         expect(first).not.toBe(second);
         expect(sw.all).toContain(first);
-        expect(sw.all).toContain(second);
-    })
-})
+        expect(sw.all).toContain(first);
+    });
+    
+    test('more than 1 random item is returned', () => {
+        let fourRandomArray = sw.random(4);
+        expect(fourRandomArray.length).toBe(4);
+        fourRandomArray.forEach(name => {
+            expect(sw.all).toContain(name);
+        });
 
-
+    });
+});
